406d8f1e99a40ded2af88346669fa4949f15f8de
kittiejanae <talkjay31@gmail.com>
2021-03-28 18:56:08 -0400

    added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  var titles = [
+    "Let That Sh*t Go",
+    "You Are Allowed to Feel This Way",
+    "Your Feelings Are Valid",
+    "Do Not Let This Fester",
+    "Forgive Yourself",
+    "You Don't Have to Carry This",
+    "There's Darkness, But There's Light Too",
+    "Speak Your Truth",
+    "Say What You Need To Say",
+    "We're Here to Listen",
+    "This is YOUR Story. You're the Main Character.",
+    "It Takes a Lot of Strength to Let Go",
+    "Drop That Pain & Never Look Back",
+    "Shout, Scream, Shake It, Let It Go",
+    "Sometimes Sharing Your Pain Is Exactly What You Need",
+    "This is You Choosing To Be Brave",
+    "Your Pain is the Breaking of the Shell That Encloses Your Understanding",
+    "You Aren't Alone",
+    "Don't Stay Here, Push Forward",
+    "No Need to Bite Your Tongue Here",
+    "You Don't Have to Face This Silently",
+  ];
+  var title = titles[Math.floor(Math.random() * titles.length)]; // pick random item
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <p>
+        Welcome, friend. This is where anonymity meets compassion. Pour your
+        heart out in this safe space. No judgements. Scream into the void.
+      </p>
+      <h1>{title}</h1>
+      <div className="post">
+        <input type="textarea" />
+        <button>RELEASE</button>
+      </div>
+      <section>
+        <div className="submissions">
+          <p className="sub-text">post goes here</p>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import screamer from "../images/Screamer.svg";
+
+export default function Header() {
+  return (
+    <>
+      <div>
+        <img src={screamer} type="img/svg" alt="logo" />
+      </div>
+    </>
+  );
+}
